93dd1714b3e6ebe89e201ed58b2a7eee5b130c60
Kenneth Kousen <ken.kousen@kousenit.com>
2016-02-16 13:57:58 -0500

    Update stadium data

--- src/main/groovy/nfjs/geocoder/populate_stadium_data.groovy
+++ src/main/groovy/nfjs/geocoder/populate_stadium_data.groovy
@@ -35,7 +35,7 @@ stadiums << new Stadium(name:'Rogers Centre',street:'One Blue Jays Way',city:'To
 stadiums << new Stadium(name:'Nationals Park',street:'1500 South Capitol Street SE',city:'Washington',state:'DC',team:'was')
 
 Sql db = Sql.newInstance(
-    url:'jdbc:h2:build/baseball',driver:'org.h2.Driver')
+    url:'jdbc:h2:build/baseball', driver:'org.h2.Driver')
 
 db.execute "drop table if exists stadium;"
 db.execute '''
