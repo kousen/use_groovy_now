90089bfbfff7fda9f013786e34661b771c6eeabd
Kenneth Kousen <ken.kousen@kousenit.com>
2017-02-23 12:01:14 -0500

    Update versions in build, and minor updates on code

--- src/main/groovy/nfjs/ast/delegate/smart_phone.groovy
+++ src/main/groovy/nfjs/ast/delegate/smart_phone.groovy
@@ -1,22 +1,32 @@
 package nfjs.ast.delegate
 
 class Phone {
+    String from
+
     String dial(String num) {
         "Dialing $num..."
     }
 }
 
 class Camera {
+    String from
+
     String takePicture() {
         'Taking picture...'
     }
 }
 
 class SmartPhone {
-    @Delegate Phone phone = new Phone()
-    @Delegate Camera camera = new Camera()
+    @Delegate Phone phone = new Phone(from: 'Samsung')
+    @Delegate Camera camera = new Camera(from: 'Nikon')
+
+    String getFrom() {
+        "Phone: ${phone.from}, Camera: ${camera.from}"
+    }
 }
 
 SmartPhone sp = new SmartPhone()
 assert sp.dial('555-1234') == 'Dialing 555-1234...'
 assert sp.takePicture() == 'Taking picture...'
+
+println sp.getFrom()
