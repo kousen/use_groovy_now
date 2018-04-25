69fee8f7244dac58d76d28c26c040ad8c940ebaa
Ken Kousen <ken.kousen@kousenit.com>
2018-04-25 09:17:57 +0530

    Update key for geocoder

--- src/main/groovy/nfjs/geocoder/Geocoder.groovy
+++ src/main/groovy/nfjs/geocoder/Geocoder.groovy
@@ -3,13 +3,14 @@ package nfjs.geocoder
 class Geocoder {
     public static final String BASE =
             'https://maps.googleapis.com/maps/api/geocode/xml?'
+    private static final String KEY = 'AIzaSyCFO7tVVWBnEhNpnjZOBOrXOav1bbusF5I'
 
     Stadium fillInLatLng(Stadium stadium) {
         String encoded = [stadium.street, stadium.city,
                           stadium.state].collect {
             URLEncoder.encode(it, 'UTF-8')
         }.join(',')
-        String qs = "address=$encoded"
+        String qs = "address=$encoded&key=$KEY"
         def root = new XmlSlurper().parse("$BASE$qs")
         def loc = root.result[0].geometry.location
         stadium.latitude = loc.lat.toBigDecimal()
